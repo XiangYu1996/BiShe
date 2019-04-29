bebae19e4e67a36096ea8d6728a49f6ae6db3dac
liu xiangyu <xyliucp@isoftstone.com>
2019-04-29 17:31:29 +0800

    没有老师开启签到时，隐藏的提示出现

--- app/src/main/java/com/example/testbishe/QiandaoFragment.java
+++ app/src/main/java/com/example/testbishe/QiandaoFragment.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +17,7 @@ public class QiandaoFragment extends Fragment {
     List<ItemQiandao> mList = new ArrayList<>();
     RecyclerView mRecyclerView;
     ItemQiandaoAdapter mItemQiandaoAdapter;
+    TextView qiandao_recycle_null;
 
     public QiandaoFragment(){
         // Required empty public constructor
@@ -26,18 +29,24 @@ public class QiandaoFragment extends Fragment {
         // Inflate the layout for this fragment
 
         View view = inflater.inflate(R.layout.qiandao_fragment, container, false);
+        qiandao_recycle_null = (TextView)view.findViewById(R.id.qiandao_recycler_null);
+if (mList==null){
+    mRecyclerView.setVisibility(View.GONE);
+    qiandao_recycle_null.setVisibility(View.VISIBLE);
 
 
-        mRecyclerView = view.findViewById(R.id.qiandao_recyclerView);
-        mItemQiandaoAdapter = new ItemQiandaoAdapter(mList);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this.getContext());
-        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        mRecyclerView.setLayoutManager(layoutManager);
-        mRecyclerView.addItemDecoration(new RecycleViewDivider(this.getContext(), LinearLayoutManager.VERTICAL));
-        mRecyclerView.setAdapter(mItemQiandaoAdapter);
-
+}else {
+    mRecyclerView = view.findViewById(R.id.qiandao_recyclerView);
+    mItemQiandaoAdapter = new ItemQiandaoAdapter(mList);
+    LinearLayoutManager layoutManager = new LinearLayoutManager(this.getContext());
+    layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+    mRecyclerView.setLayoutManager(layoutManager);
+    mRecyclerView.addItemDecoration(new RecycleViewDivider(this.getContext(), LinearLayoutManager.VERTICAL));
+    mRecyclerView.setAdapter(mItemQiandaoAdapter);
+    mRecyclerView.setVisibility(View.VISIBLE);
+    qiandao_recycle_null.setVisibility(View.GONE);
+}
         initList();
-
         return view;
     }
 
@@ -46,6 +55,7 @@ public class QiandaoFragment extends Fragment {
             ItemQiandao messi = new ItemQiandao("03-111", "高数", "胡明");
             mList.add(messi);
         }
+        mList.add(new ItemQiandao("0000022", "数据结构", "胡鸣"));
     }
 
     @Override
