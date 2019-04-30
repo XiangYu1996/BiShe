2eda62516a7da199e4e3d35cf5d0a5884ab46567
liu xiangyu <xyliucp@isoftstone.com>
2019-04-30 15:01:05 +0800

    新建    教师开启签到界面  添加获取GPS功能

---
+++ app/src/main/java/com/example/testbishe/TeacherNewQiaodao.java
@@ -0,0 +1,134 @@
+package com.example.testbishe;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.example.testbishe.QianDao.ItemQiandaoAdapter;
+
+public class TeacherNewQiaodao extends AppCompatActivity implements View.OnClickListener {
+
+
+    private LocationClient mLocationClient;
+    private BDAbstractLocationListener mBDLocationListener;
+    private double latitude;
+    private double longitude;
+    private String address;
+
+
+    private TextView show_jiangdu;
+    private TextView show_weidu;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.teacher_new_qiandao);
+
+        Button getgps = (Button) findViewById(R.id.get_gps);
+        show_jiangdu = (TextView) findViewById(R.id.show_jingdu);
+        show_weidu = (TextView) findViewById(R.id.show_weidu);
+
+        // 声明LocationClient类
+        mLocationClient = new LocationClient(getApplicationContext());
+        mBDLocationListener = new MyBDLocationListener();
+
+        // 注册监听
+        mLocationClient.registerLocationListener(mBDLocationListener);
+        //获取地址
+        getLocation();
+
+        getgps.setOnClickListener(this);
+
+    }
+
+
+    /** 获得所在位置经纬度及详细地址 */
+    public void getLocation(){
+        // 声明定位参数  
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);// 设置定位模式 高精度  
+        option.setCoorType("bd09ll");// 设置返回定位结果是百度经纬度 默认gcj02  
+        option.setScanSpan(5000);// 设置发起定位请求的时间间隔 单位ms  
+        option.setIsNeedAddress(true);// 设置定位结果包含地址信息  
+        option.setNeedDeviceDirect(true);// 设置定位结果包含手机机头 的方向  
+        // 设置定位参数  
+        mLocationClient.setLocOption(option);
+        // 启动定位  
+        mLocationClient.start();
+    }
+
+    @Override
+    public void onClick(View v) {
+//        // 声明LocationClient类
+//        mLocationClient = new LocationClient(v.getContext());
+//        mBDLocationListener = new MyBDLocationListener();
+//
+//        // 注册监听
+//        mLocationClient.registerLocationListener(mBDLocationListener);
+//        //获取地址
+
+//        getLocation();
+
+
+        if (latitude != 0) {
+
+            show_jiangdu.setText(String.valueOf(latitude));
+            show_weidu.setText(String.valueOf(longitude));
+//            Toast.makeText(v.getContext(),"  经度"+latitude
+//                    +"纬度"+longitude+address, Toast.LENGTH_SHORT).show();
+
+
+            //使用后要注销监听，否则出错---经度:4.9E-324 纬度:4.9E-324
+            mLocationClient.unRegisterLocationListener(mBDLocationListener);
+
+        }else {
+            Toast.makeText(v.getContext(), "GPS正在定位", Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+
+    private class MyBDLocationListener extends BDAbstractLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+            // 非空判断
+            if (bdLocation != null) {
+                // 根据BDLocation 对象获得经纬度以及详细地址信息
+                latitude = bdLocation.getLatitude();
+                longitude = bdLocation.getLongitude();
+                address = bdLocation.getAddrStr();
+                //获取定位类型、定位错误返回码，具体信息可参照类参考中BDLocation类中的说明
+                int errorCode = bdLocation.getLocType();
+                Log.v("GPS",  " 经度:" + latitude
+                        + " 纬度:" + longitude + "—"+"获取定位类型、定位错误返回码"+errorCode+" "+address);
+                //Toast.makeText(getContext(), " 经度:" + latitude + " 纬度:" + longitude + "—", Toast.LENGTH_SHORT).show();
+
+            }
+            if (mLocationClient.isStarted()) {
+                // 获得位置之后停止定位
+                mLocationClient.stop();
+            }
+        }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mBDLocationListener!=null){
+            mLocationClient.stop();
+        }
+    }
+}
